a173aedebeef1e87170f5e436fe2ba51c86e068b
isaychris <adaptxsurvive@gmail.com>
2018-08-24 01:09:18 -0700

    added multithreaded pinging

--- osrs_world_pinger.py
+++ osrs_world_pinger.py
@@ -1,8 +1,13 @@
 from bs4 import BeautifulSoup
+from threading import Thread
 import subprocess
 import collections
 import requests
+import queue
 import re
+
+num_threads = 16
+ping_queue = queue.Queue()
 
 url = "http://oldschool.runescape.com/slu?order=WMLPA"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
@@ -13,12 +18,17 @@ table = soup.findAll("tr", "server-list__row")
 
 server_list = {}
 
-def get_ping(world):
-    address = "oldschool{}.runescape.com".format(world)
-    command = "ping {} -n 1".format(address)
-    output = subprocess.check_output(command, shell=True).decode('utf-8')
-    matches = re.findall("time=([\d.]+)ms", output)
-    return int(matches[0])
+# thread code : wraps system ping command
+def thread_pinger(i, q):
+    while True:
+        world = q.get()
+        address = "oldschool{}.runescape.com".format(world)
+
+        command = "ping {} -n 1".format(address)
+        output = subprocess.check_output(command, shell=True).decode('utf-8')
+        matches = re.findall("time=([\d.]+)ms", output)
+        server_list[world]['ping'] = int(matches[0])
+        q.task_done()
 
 def init_server_list():
     for row in table:
@@ -27,10 +37,6 @@ def init_server_list():
         p = data[1].text.split()[0]
         c, t, a, ping = data[2].text, data[3].text, data[4].text, None
         server_list[w] = {"players": p, "country": c, "type": t, "activity": a, "ping": 0}
-
-def get_server_info(w):
-    server_list[w]['ping'] = get_ping(w)
-    return server_list[w]
 
 def get_best_servers():
     print("Top Five Worlds: \n")
@@ -58,21 +64,43 @@ def main():
     print("---------------------------------------------------------------------------------------")
 
     if x == "":
+        for key in sorted(server_list.keys()):
+            ping_queue.put(key)
+
+        # start the thread pool
+        for i in range(num_threads):
+            worker = Thread(target=thread_pinger, args=(i, ping_queue))
+            worker.setDaemon(True)
+            worker.start()
+
+        # wait until worker threads are done to exit
+        ping_queue.join()
+
         for key, value in server_list.items():
-            server = get_server_info(key)
-            print("{:<7} {:<20} {:<15} {:<15} {:<10} {}".format(key, server["country"], server["players"],
-                                                                server["type"], server["ping"], server["activity"]))
+            print("{:<7} {:<20} {:<15} {:<15} {:<10} {}".format(key, value["country"], value["players"],
+                                                                value["type"], value["ping"], value["activity"]))
 
         print("=======================================================================================")
-
         get_best_servers()
 
     else:
         if x in server_list:
-            server = get_server_info(x)
-            print("{:<7} {:<20} {:<15} {:<15} {:<10} {}".format(x, server["country"], server["players"],
-                                                                server["type"], server["ping"], server["activity"]))
+            ping_queue.put(x)
+
+            # start the thread pool
+            for i in range(num_threads):
+                worker = Thread(target=thread_pinger, args=(i, ping_queue))
+                worker.setDaemon(True)
+                worker.start()
+
+            # wait until worker threads are done to exit
+            ping_queue.join()
+            print("{:<7} {:<20} {:<15} {:<15} {:<10} {}".format(x, server_list[x]["country"],server_list[x]["players"],
+                                                                server_list[x]["type"], server_list[x]["ping"],
+                                                                server_list[x]["activity"]))
 
         else:
             print("Unable to retrieve info for world [{}] ...".format(x))
-main()
+			
+if __name__ == "__main__":
+    main()
